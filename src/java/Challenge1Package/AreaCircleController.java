e28ffc9362ccb40df60fb92df194d0589d9de4f4
jlatz1 <jlatz1@L11710038610.wctc.msft>
2013-01-30 18:45:53 -0600

    more changes to areaCircle

--- src/java/Challenge1Package/AreaCircleController.java
+++ src/java/Challenge1Package/AreaCircleController.java
@@ -36,20 +36,7 @@ public class AreaCircleController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AreaCircleController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AreaCircleController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {            
-            out.close();
-        }
+       
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
